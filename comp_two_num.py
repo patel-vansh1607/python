4acfbbe7c7a48866f2198c713af075a4d0824f27
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-23 16:38:21 +0300

    added

---
+++ comp_two_num.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter a number 1: "))
+num2 = int(input("Enter a number 2 "))
+
+if num1 < num2:
+    print("Number 1 is less than Number 2")
+elif num1 > num2:
+    print("Number 2 is greater than Number 1")
+else:
+    print("Invalid number")
