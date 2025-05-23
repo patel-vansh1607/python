86050b446bbcdc3bc7f50ee14cd04e08f70a5459
Vansh Patel <patel.vansh1607@gmail.com>
2025-05-23 16:42:01 +0300

    ADDED

---
+++ traffic_light.py
@@ -0,0 +1,10 @@
+light = str(input("Enter a color. Hint: Enter Red, Yellow or Green: "))
+
+if light == "Red":
+    print("Stop")
+elif light == "Yellow":
+    print("Get Ready")  
+elif light == "Green":
+    print("Go")
+else:
+    print("Invalid Color")
